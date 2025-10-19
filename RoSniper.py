35ab0b79ad98315b517548bdcc44eb8cde980a60
Aaron Wijesinghe <aaronw1975@icloud.com>
2025-10-19 15:00:16 -0400

    feat: display error if donations.json is missing

--- RoSniper.py
+++ RoSniper.py
@@ -347,6 +347,10 @@ def run_command(command):
                 wait(1, f"\n{underline}Successfully updated the <snipe> alias.{end}")
                 return
     elif command.startswith("/donate "):
+        if not os.path.exists("./assets/donations.json"):
+            wait(1, f"{nl}{underline}The file ./assets/donations.json isn't present.{end}")
+            return
+
         donations = json.loads(open("./assets/donations.json").read())
         if arg in donations.keys():
             print(f"{nl}{underline}Thank you for donating! Donations help keep RoSniper free for all! - @Awij126{end}")
